f8eb620887f0d10806aa09bcea15d0a807dee5f9
Abby.041b <abby.041b@gmail.com>
2024-07-21 17:59:03 -0400

    Camera support (in Scene)

--- src/nodes/Scene.ts
+++ src/nodes/Scene.ts
@@ -1,3 +1,4 @@
+import { Camera } from './Camera';
 import { PNode } from './PNode';
 
 /**
@@ -7,12 +8,37 @@ import { PNode } from './PNode';
  */
 export class Scene extends PNode {
   private static currSceneID = 0;
-
   public readonly sceneID: number;
+
+  public cameras: WeakRef<Camera>[] = [];
 
   /** Instantiates a scene */
   public constructor() {
     super();
     this.sceneID = Scene.currSceneID++;
+  }
+
+  /**
+   * Gets this scene's active camera.
+   * If no camera is found, this is `undefined`.
+   */
+  public getCamera(): Camera | undefined {
+    let index = -1;
+    for (const cameraRef of this.cameras) {
+      index++;
+
+      const camera = cameraRef.deref();
+      if (!camera) {
+        // Remove this camera from the list
+        this.cameras.splice(index, 1);
+        continue;
+      }
+
+      if (camera.isActive) {
+        return camera;
+      }
+    }
+
+    return undefined;
   }
 }
