990bfb1184d2bdac737ef4c832e69856517afe93
Abby.041b <abby.041b@gmail.com>
2024-07-22 16:29:48 -0400

    Parent control nodes now affect children

--- src/nodes/control/ControlNode.ts
+++ src/nodes/control/ControlNode.ts
@@ -13,7 +13,7 @@ type ControlNodeSize = [ number, SizeType ];
 interface ControlNodeParent extends PNode {
   controlProperties?: {
     /** Whether or not children are horizontally aligned */
-    horizontalAlign?: boolean,
+    horizontalAlign: boolean,
 
     /** Used to know when to re-calculate sizes */
     lastUpdateFrame: number,
@@ -33,6 +33,10 @@ export abstract class ControlNode extends PNode {
   public height: ControlNodeSize = [ 1, SizeType.FRACTION ];
   public controlProperties?: ControlNodeParent;
 
+  // Used for children to know their parent's size
+  private lastCalculatedWidth  = 0;
+  private lastCalculatedHeight = 0;
+
   /** Sets this node's width in pixels */
   public setWidthPixels(width: number): this {
     this.width[0] = width;
@@ -40,7 +44,7 @@ export abstract class ControlNode extends PNode {
     return this;
   }
   
-  /** Sets this node's width in percent (relative to the parent) */
+  /** Sets this node's width as a percent (relative to the parent) */
   public setWidthPercent(width: number): this {
     this.width[0] = width;
     this.width[1] = SizeType.PIXELS;
@@ -54,7 +58,7 @@ export abstract class ControlNode extends PNode {
     return this;
   }
   
-  /** Sets this node's height in percent (relative to the parent) */
+  /** Sets this node's height as a percent (relative to the parent) */
   public setHeightPercent(height: number): this {
     this.height[0] = height;
     this.height[1] = SizeType.PIXELS;
@@ -62,10 +66,14 @@ export abstract class ControlNode extends PNode {
   }
 
   /** Draws this control node */
-  protected draw() {
+  protected override draw() {
     const parent: PNode & ControlNodeParent = this.parent;
-    const screenWidth = Peek.screenWidth;
-    const screenHeight = Peek.screenHeight;
+    let parentWidth  = Peek.screenWidth;
+    let parentHeight = Peek.screenHeight;
+    if (parent instanceof ControlNode) {
+      parentWidth  = parent.lastCalculatedWidth ;
+      parentHeight = parent.lastCalculatedHeight;
+    }
 
     if (!parent.controlProperties) {
       // Initialize parent control properties!
@@ -104,7 +112,7 @@ export abstract class ControlNode extends PNode {
           ? child.width
           : child.height;
         if (size[1] == SizeType.PIXELS) {
-          totalSize += size[0] / screenHeight;
+          totalSize += size[0] / parentHeight;
         } else {
           totalSize += size[0];
         }
@@ -124,29 +132,28 @@ export abstract class ControlNode extends PNode {
     const transform = Peek.ctx.getTransform();
     if (parent.controlProperties!.horizontalAlign) {
       Peek.ctx.translate(
-        Math.floor(screenHeight * parent.controlProperties!.currentChildOffset),
+        Math.floor(parentHeight * parent.controlProperties!.currentChildOffset),
         0
       );
     } else {
       Peek.ctx.translate(
         0,
-        Math.floor(screenHeight * parent.controlProperties!.currentChildOffset)
+        Math.floor(parentHeight * parent.controlProperties!.currentChildOffset)
       );
     }
 
     // Call this innerDraw method
-    this.innerDraw(
-      Math.floor(widthMultiplier * (
-        this.width[1] == SizeType.FRACTION
-          ? this.width[0] * screenWidth
-          : this.width[0]
-      )),
-      Math.floor(heightMultiplier * (
-        this.height[1] == SizeType.FRACTION
-          ? this.height[0] * screenHeight
-          : this.height[0]
-      ))
-    );
+    this.lastCalculatedWidth = Math.floor(widthMultiplier * (
+      this.width[1] == SizeType.FRACTION
+        ? this.width[0] * parentWidth
+        : this.width[0]
+    ));
+    this.lastCalculatedHeight = Math.floor(heightMultiplier * (
+      this.height[1] == SizeType.FRACTION
+        ? this.height[0] * parentHeight
+        : this.height[0]
+    ));
+    this.innerDraw(this.lastCalculatedWidth, this.lastCalculatedHeight);
 
     // Un-transform
     Peek.ctx.setTransform(transform);
@@ -157,14 +164,14 @@ export abstract class ControlNode extends PNode {
         parent.controlProperties!.sizeMultiplier * (
           this.width[1] == SizeType.FRACTION
             ? this.width[0]
-            : this.width[0] / screenHeight
+            : this.width[0] / parentHeight
         );
     } else {
       parent.controlProperties!.currentChildOffset +=
         parent.controlProperties!.sizeMultiplier * (
           this.height[1] == SizeType.FRACTION
             ? this.height[0]
-            : this.height[0] / screenHeight
+            : this.height[0] / parentHeight
         );
     }
   }
