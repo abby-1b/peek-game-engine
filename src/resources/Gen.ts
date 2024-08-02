30d05986fe2a02927a4955819720c1b8dc0541fc
Abby.041b <abby.041b@gmail.com>
2024-08-01 22:36:54 -0400

    New `splatter()` procgen

--- src/resources/Gen.ts
+++ src/resources/Gen.ts
@@ -1,10 +1,19 @@
+import { pickRandom, randomRange } from '../util/math';
 import { Color } from './Color';
-import { Texture } from './Texture';
+import { atlasColor, Texture } from './Texture';
 
 interface BitNoiseData {
   colors: Color[],
-  mirrorX?: boolean
-  mirrorY?: boolean
+  mirrorX?: boolean,
+  mirrorY?: boolean,
+}
+
+interface SplatterData {
+  backgroundColor?: Color,
+  colors?: Color[],
+  splatterCount?: number,
+  widthRange?: [number, number],
+  heightRange?: [number, number],
 }
 
 /** Asset generation tool */
@@ -13,7 +22,10 @@ export class Gen {
    * Generates a texture with a given set of colors.
    * Defaults to black and white.
    */
-  public static bitNoise(width: number, height: number, data?: BitNoiseData) {
+  public static bitNoise(
+    width: number, height: number,
+    data?: BitNoiseData
+  ) {
     // Get the colors that the image will have
     const colors = data && data.colors && data.colors.length > 0
       ? data.colors
@@ -31,6 +43,43 @@ export class Gen {
     }
     
     // Return
+    return tex;
+  }
+
+  /** Splatters rects of a given size across a texture */
+  public static splatter(
+    width: number, height: number,
+    data?: SplatterData
+  ) {
+    // Get the ranges for the width/height of the rectangles
+    const widthRange = data?.widthRange ?? [ 1, (width + height) / 20 ];
+    const heightRange = data?.heightRange ?? [ 1, (width + height) / 20 ];
+
+    // Get the rectangle count
+    const avgArea =
+      ((widthRange[0] + widthRange[1]) / 2) *
+      ((heightRange[0] + heightRange[1]) / 2);
+    const rectCount = data?.splatterCount ?? (width * height / (2 * avgArea));
+
+    // Get the drawing colors
+    const colors = data?.colors ?? [ Color.WHITE ];
+
+    // Make the texture
+    const tex = new Texture(width, height);
+    tex.fill(data?.backgroundColor ?? Color.TRANSPARENT);
+
+    // Generate
+    for (let i = 0; i < rectCount; i++) {
+      const w = ~~randomRange(...widthRange);
+      const h = ~~randomRange(...heightRange);
+
+      const x = ~~randomRange(0, width - w);
+      const y = ~~randomRange(0, height - h);
+
+      atlasColor(pickRandom(colors));
+      tex.fillRect(x, y, w, h);
+    }
+
     return tex;
   }
 }
