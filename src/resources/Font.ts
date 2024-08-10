1a9389c874a224bcaacd0f1dd19e55207e860dd6
Abby.041b <abby.041b@gmail.com>
2024-08-09 23:12:39 -0400

    Font deduplication
    Fonts that load the same file now use the same object, so font atlases don't get loaded more than once.
    
    Also added the `getTextSize` method, which although incomplete works for monospaced fonts.

--- src/resources/Font.ts
+++ src/resources/Font.ts
@@ -1,7 +1,19 @@
 import { Peek } from '../peek';
+import { Path } from '../util/Path';
+import { Vec2 } from './Vec';
 
 /** Holds the data for a font */
 export class Font {
+  public static loadedFonts: Record<string, Font> = {};
+
+  public static defaultFont = Font.load(
+    Path.relativeToModule(
+      import.meta.url,
+      '../resources/font-medium.png'
+    ),
+    6, 8, false
+  );
+
   /**
    * The image containing the font. Fonts aren't loaded into the TextureAtlas,
    * which is done so they don't take up any space there.
@@ -16,24 +28,46 @@ export class Font {
   public lineSpacing = 1;
   public tabSize = 2;
 
+  /**
+   * Loads a font
+   * @param url The path to the font
+   * @param symbolWidth The individual symbol width in the font atlas
+   * @param symbolHeight The individual symbol height in the font atlas
+   * @param dynamic Whether or not the font is dynamic
+   */
+  public static load(
+    url: string,
+    symbolWidth: number,
+    symbolHeight: number,
+    isDynamic: boolean
+  ) {
+    if (url in this.loadedFonts) {
+      return this.loadedFonts[url];
+    } else {
+      const newFont = new Font(url, symbolWidth, symbolHeight, isDynamic);
+      this.loadedFonts[url] = newFont;
+      return newFont;
+    }
+  }
+
   /** Loads a font */
-  public constructor(
+  private constructor(
     url: string,
     public readonly symbolWidth: number,
     public readonly symbolHeight: number,
-    public readonly dynamic: boolean
+    public readonly isDynamic: boolean,
   ) {
     this.fontImage = new Image();
-    this.fontImage.onload = () => this.fontLoaded(true);
+    this.fontImage.onload = () => this.onFontLoaded(true);
     this.fontImage.onerror = (e) => {
       console.log(e);
-      this.fontLoaded(false);
+      this.onFontLoaded(false);
     };
     this.fontImage.src = url;
   }
 
   /** Runs when the font image either loads or errors. */
-  private fontLoaded(success: boolean) {
+  private onFontLoaded(success: boolean) {
     if (!success) {
       this.fontImage = undefined;
       return;
@@ -44,10 +78,60 @@ export class Font {
     this.symbolsPerRow = Math.floor(this.imageWidth / this.symbolWidth);
   }
 
+  /** Gets the size of a piece of text */
+  public getTextSize(text: string): Vec2 {
+    if (this.isDynamic) {
+      // TODO: implement textbox sizing for dynamic-sized fonts
+      return Vec2.zero();
+    } else {
+      const totalLineHeight = this.lineSpacing + this.symbolHeight;
+      const totalSymbolWidth = this.symbolWidth + this.symbolSpacing;
+
+      let maxLineLen = 0;
+
+      let trailingHSpace = 0;
+      let trailingVSpace = 0;
+      let lineCount = 0;
+      let lineLen = 0;
+      for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        if (char == '\n') {
+          lineLen -= trailingHSpace;
+          maxLineLen = Math.max(maxLineLen, lineLen);
+          lineLen = 0;
+          trailingHSpace = 0;
+          lineCount++;
+          trailingVSpace++;
+        } else {
+          if (char == ' ') {
+            trailingHSpace++;
+          } else {
+            if (lineCount == 0) lineCount++;
+            trailingVSpace = 0;
+          }
+          lineLen++;
+        }
+      }
+
+      lineLen -= trailingHSpace;
+      maxLineLen = Math.max(maxLineLen, lineLen);
+
+      console.log(lineCount);
+
+      // Trailing vertical space isn't counted
+      lineCount -= trailingVSpace;
+
+      return new Vec2(
+        totalSymbolWidth * maxLineLen,
+        totalLineHeight * lineCount
+      );
+    }
+  }
+
   /** Draws a string to the canvas */
   public draw(
     text: string, x: number, y: number,
-    ctx: CanvasRenderingContext2D = Peek.ctx
+    ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D = Peek.ctx
   ) {
     let currX = x;
     let currY = y;
